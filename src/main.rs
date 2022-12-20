77dff91c94be08be9360141d475b05015eb5fea3
Tom Wissman <tomwissman@MacBook-Pro-2.local>
2022-12-19 20:10:31 -0500

    Tic Tac Toe Project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate termcolor;
-
+///External crate for writing colored text to a terminal
 use std::io::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-
+///Function that outputs a greeting message
 fn greeting() {
     println!(
         "\nRust TicTacToe\n\
@@ -11,10 +11,10 @@ fn greeting() {
          Code is available at: https://github.com/flofriday/tictactoe"
     )
 }
-
+///this function creates the players
 fn print_player(player: &char) {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
-
+///Colors the input of player X
     if player == &'X' {
         stdout
             .set_color(ColorSpec::new().set_fg(Some(Color::Blue)))
@@ -28,10 +28,10 @@ fn print_player(player: &char) {
     write!(&mut stdout, "{}", player).unwrap();
     stdout.reset().unwrap();
 }
-
+///This function creates the tic tac toe board
 fn draw(state: &[char]) {
     println!("\n");
-
+///For loop which allows for the board to contiously be printed after each input
     for i in (0..3).rev() {
         let offset = i * 3;
 
@@ -46,19 +46,19 @@ fn draw(state: &[char]) {
 
     println!("-------------");
 }
-
+///Function that asks the user to pick a number located on the board
 fn ask_user(state: &mut [char], player: char) {
     loop {
         print!("Player '");
         print_player(&player);
         println!("', enter a number: ");
-
+///if statement which error handles if it cannot read the line
         let mut input = String::new();
         if std::io::stdin().read_line(&mut input).is_err() {
             println!("Couldn't read line! Try again.");
             continue;
         }
-
+///if statement that error handles if the number is outside 1-9
         if let Ok(number) = input.trim().parse::<usize>() {
             if number < 1 || number > 9 {
                 println!("The field number must be between 1 and 9.");
@@ -66,7 +66,7 @@ fn ask_user(state: &mut [char], player: char) {
             }
 
             let number = number - 1;
-
+///if statement that error handles if a spot on the board is already taken
             if state[number] == 'X' || state[number] == 'O' {
                 print!("This field is already taken by '");
                 print_player(&state[number]);
@@ -75,7 +75,7 @@ fn ask_user(state: &mut [char], player: char) {
             }
 
             state[number] = player;
-
+///Else statement if there is invalid input which prints out an error message
             break;
         } else {
             println!("Only numbers are allowed.");
@@ -83,7 +83,7 @@ fn ask_user(state: &mut [char], player: char) {
         }
     }
 }
-
+///Function that checks if the user has won or not
 fn has_won(state: &[char]) -> bool {
     for tmp in 0..3 {
         if state[tmp] == state[tmp + 3] && state[tmp] == state[tmp + 6] {
@@ -105,7 +105,7 @@ fn has_won(state: &[char]) -> bool {
 
     false
 }
-
+///This is a function that will check if all of the fields are used
 #[inline(always)]
 fn is_over(state: &[char]) -> bool {
     state.iter().all(|&v| v == 'X' || v == 'O')
@@ -115,17 +115,17 @@ fn main() {
     let mut state = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let mut player = 'X';
 
-    // Welcome the player
+    ///Calls the greeting function which greets player
     greeting();
 
     loop {
-        // Draw the field
+        ///Calls the draw function which creates the board
         draw(&state);
 
-        // Ask for user input
+        ///Calls the ask_user function which asks the user for input
         ask_user(&mut state, player);
 
-        // Check if a player won
+        ///Calls the has_won function to check if a user has won
         if has_won(&state) {
             draw(&state);
             print!("Player '");
@@ -134,14 +134,13 @@ fn main() {
             break;
         }
 
-        // Check if all fields are used
+        ///is_over function that will check if the gamehas ended 
         if is_over(&state) {
             draw(&state);
             println!("All fields are used. No one won. (._.)");
             break;
         }
 
-        // Switch player
         player = if player == 'X' { 'O' } else { 'X' }
     }
 }
